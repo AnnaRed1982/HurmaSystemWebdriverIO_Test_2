c609f173704bfa7613f31154f9d95f348aa3627c
annared <annared1982@gmail.com>
2023-12-15 16:43:09 +0200

    add test

---
+++ test/pageobjects/tariffs.page.js
@@ -0,0 +1,10 @@
+const { $ } = require("@wdio/globals");
+const Page = require("./page");
+
+class TariffsPage extends Page {
+  open() {
+    return super.open("tariffs/");
+  }
+}
+
+module.exports = new TariffsPage();
---
+++ test/specs/test.e2e.js
@@ -0,0 +1,76 @@
+const { expect } = require("@wdio/globals");
+const TariffsPage = require("../pageobjects/tariffs.page");
+// const LoginPage = require('../pageobjects/login.page')
+// const SecurePage = require('../pageobjects/secure.page')
+
+// async function priceOnChangeWorkersQuant() {
+//   await (await $(".tariff-card.primary").$(".price-value")).getText();
+// }
+
+describe("tarriffs page test", () => {
+  it("checking tariff-card primary tarrifs", async () => {
+    await TariffsPage.open();
+
+    let select = await $(".tariff-card.primary").$("select");
+    let options = await select.$$("option");
+
+    expect(options.length).toBe(8);
+
+    for (let i = 0; i < options.length; i++) {
+      await select.selectByIndex(i);
+      await select.click();
+
+      switch (i) {
+        case 0:
+          //   expect(priceOnChangeWorkersQuant()).toEqual("103.5");
+          expect(
+            await (await $(".tariff-card.primary").$(".price-value")).getText()
+          ).toEqual("103.5");
+          break;
+
+        // case 1:
+        //   expect(
+        //     await (await $(".tariff-card.primary").$(".price-value")).getText()
+        //   ).toEqual("157.5");
+        //   break;
+
+        // case 2:
+        //   expect(
+        //     await (await $(".tariff-card.primary").$(".price-value")).getText()
+        //   ).toEqual("220.5");
+        //   break;
+
+        // case 3:
+        //   await spanPriceValueText.isEqual("283.5");
+        //   break;
+
+        // case 4:
+        //   await spanPriceValueText.isEqual("310.5");
+        //   break;
+
+        // case 5:
+        //   await spanPriceValueText.isEqual("378");
+        //   break;
+
+        // case 6:
+        //   await spanPriceValueText.isEqual("445.5");
+        //   break;
+
+        // case 7:
+        //   // spanPriceValueText.isEqual("Зв'яжіться з нами для розрахунку");
+        //   console.log("Invalid select option");
+        //   break;
+
+        default:
+          console.log("Invalid select option");
+      }
+    }
+
+    // console.log("select--------------->", select);
+
+    // await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+    // await expect(SecurePage.flashAlert).toBeExisting()
+    // await expect(SecurePage.flashAlert).toHaveTextContaining(
+    //     'You logged into a secure area!')
+  });
+});
