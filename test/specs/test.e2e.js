e8e213ea8a1ae8946b3614fc0bad6e2f9e93b7ce
annared <annared1982@gmail.com>
2023-12-18 13:39:32 +0200

    Update test.e2e.js

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -10,121 +10,121 @@ const TariffsPage = require("../pageobjects/tariffs.page");
 // }
 
 describe("tarriffs page test", () => {
-//   it("tariff-card primary tarrifs change check", async () => {
-//     await TariffsPage.open();
+    it("tariff-card primary tarrifs change check", async () => {
+      await TariffsPage.open();
 
-//     let select = await $(".tariff-card.primary").$("select");
-//     let options = await select.$$("option");
+      let select = await $(".tariff-card.primary").$("select");
+      let options = await select.$$("option");
 
-//     expect(options.length).toBe(8);
+      expect(options.length).toBe(8);
 
-//     for (let i = 0; i < options.length; i++) {
-//       await select.selectByIndex(i);
-//       await select.click();
+      for (let i = 0; i < options.length; i++) {
+        await select.selectByIndex(i);
+        await select.click();
 
-//       switch (i) {
-//         case 0:
-//           expect(
-//             await (await $(".tariff-card.primary").$(".price-value")).getText()
-//           ).toEqual("103.5");
-//           break;
+        switch (i) {
+          case 0:
+            expect(
+              await (await $(".tariff-card.primary").$(".price-value")).getText()
+            ).toEqual("103.5");
+            break;
 
-//         case 1:
-//           expect(
-//             await (await $(".tariff-card.primary").$(".price-value")).getText()
-//           ).toEqual("157.5");
-//           break;
+          case 1:
+            expect(
+              await (await $(".tariff-card.primary").$(".price-value")).getText()
+            ).toEqual("157.5");
+            break;
 
-//         case 2:
-//           expect(
-//             await (await $(".tariff-card.primary").$(".price-value")).getText()
-//           ).toEqual("220.5");
-//           break;
+          case 2:
+            expect(
+              await (await $(".tariff-card.primary").$(".price-value")).getText()
+            ).toEqual("220.5");
+            break;
 
-//         case 3:
-//           expect(
-//             await (await $(".tariff-card.primary").$(".price-value")).getText()
-//           ).toEqual("283.5");
-//           break;
+          case 3:
+            expect(
+              await (await $(".tariff-card.primary").$(".price-value")).getText()
+            ).toEqual("283.5");
+            break;
 
-//         case 4:
-//           expect(
-//             await (await $(".tariff-card.primary").$(".price-value")).getText()
-//           ).toEqual("310.5");
-//           break;
+          case 4:
+            expect(
+              await (await $(".tariff-card.primary").$(".price-value")).getText()
+            ).toEqual("310.5");
+            break;
 
-//         case 5:
-//           expect(
-//             await (await $(".tariff-card.primary").$(".price-value")).getText()
-//           ).toEqual("378");
-//           break;
+          case 5:
+            expect(
+              await (await $(".tariff-card.primary").$(".price-value")).getText()
+            ).toEqual("378");
+            break;
 
-//         case 6:
-//           expect(
-//             await (await $(".tariff-card.primary").$(".price-value")).getText()
-//           ).toEqual("445.5");
-//           break;
+          case 6:
+            expect(
+              await (await $(".tariff-card.primary").$(".price-value")).getText()
+            ).toEqual("445.5");
+            break;
 
-//         case 7:
-//           expect(
-//             await (
-//               await $(".tariff-card.primary")
-//                 .$(".individual-price")
-//                 .$(".price-title")
-//             ).getText()
-//           ).toEqual("Індивідуальна ціна");
-//           break;
+          case 7:
+            expect(
+              await (
+                await $(".tariff-card.primary")
+                  .$(".individual-price")
+                  .$(".price-title")
+              ).getText()
+            ).toEqual("Індивідуальна ціна");
+            break;
 
-//         default:
-//           console.log("Invalid select option");
-//       }
-//     }
+          default:
+            console.log("Invalid select option");
+        }
+      }
 
-//     // await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-//     // await expect(SecurePage.flashAlert).toBeExisting()
-//     // await expect(SecurePage.flashAlert).toHaveTextContaining(
-//     //     'You logged into a secure area!')
-//   });
+      // await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+      // await expect(SecurePage.flashAlert).toBeExisting()
+      // await expect(SecurePage.flashAlert).toHaveTextContaining(
+      //     'You logged into a secure area!')
+    });
 
-//   ///////////////////////////////////////////////////////////////////////
-//   //////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
 
-//   it("currency symbol change check", async () => {
-//     await TariffsPage.open();
+    it("currency symbol change check", async () => {
+      await TariffsPage.open();
 
-//     let select = await $(".currency-select").$("select");
-//     let options = await select.$$("option");
+      let select = await $(".currency-select").$("select");
+      let options = await select.$$("option");
 
-//     expect(options.length).toBe(3);
+      expect(options.length).toBe(3);
 
-//     for (let i = 0; i < options.length; i++) {
-//       await select.selectByIndex(i);
-//       await select.click();
+      for (let i = 0; i < options.length; i++) {
+        await select.selectByIndex(i);
+        await select.click();
 
-//       switch (i) {
-//         case 0:
-//           expect(
-//             await (await $(".price-per-month").$(".currency")).getText()
-//           ).toEqual("€");
-//           break;
+        switch (i) {
+          case 0:
+            expect(
+              await (await $(".price-per-month").$(".currency")).getText()
+            ).toEqual("€");
+            break;
 
-//         case 1:
-//           expect(
-//             await (await $(".price-per-month").$(".currency")).getText()
-//           ).toEqual("$");
-//           break;
+          case 1:
+            expect(
+              await (await $(".price-per-month").$(".currency")).getText()
+            ).toEqual("$");
+            break;
 
-//         case 2:
-//           expect(
-//             await (await $(".price-per-month").$(".currency")).getText()
-//           ).toEqual("₴");
-//           break;
+          case 2:
+            expect(
+              await (await $(".price-per-month").$(".currency")).getText()
+            ).toEqual("₴");
+            break;
 
-//         default:
-//           console.log("Invalid select option");
-//       }
-//     }
-//   });
+          default:
+            console.log("Invalid select option");
+        }
+      }
+    });
 
   ///////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////
@@ -132,12 +132,61 @@ describe("tarriffs page test", () => {
   it("Period control check", async () => {
     await TariffsPage.open();
 
-    let period = await $(".period-and-currency")
+    //open page
+    expect(
+      await (
+        await $(".period-and-currency")
+          .$(".period-control")
+          .$(".period-element.active")
+      ).getText()
+    ).toEqual("3 місяці");
+
+    //1 month
+    await $(".period-and-currency")
       .$(".period-control")
-      .$(".period-element.active");
+      .$("div=1 місяць")
+      .click();
 
-    expect(await period.getText()).toEqual("3 місяці");
+    expect(
+      await (
+        await $(".period-and-currency").$(".period-control").$("div=1 місяць")
+      ).getAttribute("class")
+    ).toEqual("period-element active");
 
-    await $(".period-and-currency").$(".period-control").$("div=1 місяць");
-  }).click();
+    //3 month
+    await $(".period-and-currency")
+      .$(".period-control")
+      .$("div=3 місяці")
+      .click();
+
+    expect(
+      await (
+        await $(".period-and-currency").$(".period-control").$("div=3 місяці")
+      ).getAttribute("class")
+    ).toEqual("period-element active");
+
+    //6 month
+    await $(".period-and-currency")
+      .$(".period-control")
+      .$("div=6 місяців")
+      .click();
+
+    expect(
+      await (
+        await $(".period-and-currency").$(".period-control").$("div=6 місяців")
+      ).getAttribute("class")
+    ).toEqual("period-element active");
+
+    //12 month
+    await $(".period-and-currency")
+      .$(".period-control")
+      .$("div=12 місяців")
+      .click();
+
+    expect(
+      await (
+        await $(".period-and-currency").$(".period-control").$("div=12 місяців")
+      ).getAttribute("class")
+    ).toEqual("period-element active");
+  });
 });
